ce25c44ec02a541573906a24a117a1aac59f9218
infyf <138444902+infyf@users.noreply.github.com>
2025-06-30 10:35:23 +0300

    Update Program.cs

--- backend/bild/Program.cs
+++ backend/bild/Program.cs
@@ -1,24 +1,24 @@
 using bild.Data;
 using Microsoft.EntityFrameworkCore;
-using System.Text.Json.Serialization; // Додай для ReferenceHandler
+using System.Text.Json.Serialization; 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Додаємо контролери з ігноруванням циклів
+
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
     options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
 });
 
-// Swagger для тестування API
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Підключення до бази даних
+
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Додаємо політику CORS
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Увімкнення CORS
+
 app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
